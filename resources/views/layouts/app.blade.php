ef9304a8f963d7c5b816c3649d3fc188001899ac
Bülent Sakarya <sakarya.bulent@gmail.com>
2023-06-23 18:32:00 +0300

    csrf

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang="tr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf_token" content="{{ csrf_token() }}">
     <title>Herkobi Panel</title>
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
     @yield('css')
 </head>
+
 <body class="app">
     <div class="wrapper">
         @include('layouts.partials.sidebar')
@@ -21,4 +24,5 @@
     @include('layouts.partials.footer')
     @yield('js')
 </body>
+
 </html>
