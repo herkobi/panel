84e1f42201154f089d0c1e1f8972bcbdcda7cc67
Bülent Sakarya <sakarya.bulent@gmail.com>
2023-07-22 21:56:44 +0300

    Role Göre Erişim Verme

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,11 @@ Route::redirect('/', 'login');
 
 Route::middleware(['auth', 'auth.session', 'verified', 'panel_settings'])->prefix('panel')->name('panel.')->group(function () {
 
-    Route::controller(Dashboard::class)->group(function () {
-        Route::get('/', 'index')->name('home');
-        Route::get('/passive', 'passive')->name('passive');
+    Route::group(['middleware' => ['role:User']], function () {
+        Route::controller(Dashboard::class)->group(function () {
+            Route::get('/', 'index')->name('home');
+            Route::get('/passive', 'passive')->name('passive');
+        });
     });
 
     Route::controller(SettingController::class)->group(function () {
